ff9f92714df993beef3900f3b2d9c1c95ccfedb0
Aviv <aviv641999@gmail.com>
2023-08-14 23:29:09 +0300

    Build free search route

--- WebServer/publisher.go
+++ WebServer/publisher.go
@@ -1,20 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
+
 	r := gin.Default()
 
-	// Ping test
+	// Ping test route
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
+
+	// Search song route
+	r.GET("/search/all/:freetext", func(c *gin.Context) {
+		textToSearch := c.Params.ByName("freetext")
+		//textToSearch := c.Params.ByName("freetext")
+		f, err := os.Open("C:\\Users\\Aviv\\Desktop\\Aviv\\Cyber\\איציק\\Code\\ExcelDB\\SongsMetadata.txt")
+		if err != nil {
+			c.String(http.StatusOK, "Do Nothing")
+		}
+		defer f.Close()
+
+		// Splits on newlines by default.
+		scanner := bufio.NewScanner(f)
+
+		line := 1
+		// https://golang.org/pkg/bufio/#Scanner.Scan
+		for scanner.Scan() {
+			if strings.Contains(scanner.Text(), textToSearch) {
+				c.String(http.StatusOK, "%s\n", strconv.Itoa(line))
+			}
+
+			line++
+		}
+
+		if err := scanner.Err(); err != nil {
+			// Handle the error
+		}
+
+		msg := "Bob Dylan"
+
+		c.String(http.StatusOK, msg)
+	})
+
 	return r
 }
 
