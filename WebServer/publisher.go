f3e37c44887a9b682179e967091f4b87b1076072
Aviv <aviv641999@gmail.com>
2023-08-14 20:15:39 +0300

    Using gin for webserver

--- WebServer/publisher.go
+++ WebServer/publisher.go
@@ -1,6 +1,25 @@
 package main
-import ("fmt")
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+	return r
+}
 
 func main() {
-  fmt.Println("Hello World!")
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
 }
